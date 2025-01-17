bc35af21e353abdffc89e08c0b601fb210c0fc7e
tompol1123581321 <tom.polivka96@gmail.com>
2025-01-17 18:43:42 +0100

    lts

--- src/components/body/public-web-settings/PublicWebSettings.tsx
+++ src/components/body/public-web-settings/PublicWebSettings.tsx
@@ -44,7 +44,7 @@ export const PublicWebSettings: React.FC = () => {
 
   useEffect(() => {
     fetchWebSettings();
-  }, [fetchWebSettings]);
+  }, []);
 
   // Sledování změn ve formuláři pro povolení tlačítka Uložit
   const handleFormChange = () => {
